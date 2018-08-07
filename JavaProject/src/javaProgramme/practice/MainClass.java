316157a0ae88963cf06da9f107aefe4c3d4912c7
Nagesh Mahadev Kadam <knagesh9841@yahoo.com>
2018-08-07 22:43:42 +0530

    reset 2

--- JavaProject/src/javaProgramme/practice/MainClass.java
+++ JavaProject/src/javaProgramme/practice/MainClass.java
@@ -12,6 +12,7 @@ public class MainClass {
 		System.out.println("Verifying stash");
 		System.out.println("Stash changes in Local");
 		System.out.println("Git reset 1");
+		System.out.println("Git reset 2");
 
 	}
 
