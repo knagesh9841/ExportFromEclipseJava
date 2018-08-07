96fbf6cecff910104b9daa4351d2cb6e173bce15
Nagesh Mahadev Kadam <knagesh9841@yahoo.com>
2018-08-07 22:46:32 +0530

    reset 4

--- JavaProject/src/javaProgramme/practice/MainClass.java
+++ JavaProject/src/javaProgramme/practice/MainClass.java
@@ -14,6 +14,7 @@ public class MainClass {
 		System.out.println("Git reset 1");
 		System.out.println("Git reset 2");
 		System.out.println("Git Reset 3");
+		System.out.println("Git Reset 4");
 
 	}
 
